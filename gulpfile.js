4d1c1a2e6dd29ad2ca713f85078249abf241353a
Alexey Lashkin <a-lashkin@ya.ru>
2019-07-14 18:36:52 +0300

    Add notifications in system; error handlers

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,29 @@ const {
 const del = require('del');
 const pug = require('gulp-pug');
 const stylus = require('gulp-stylus');
+const notify = require('gulp-notify');
 const browserSync = require('browser-sync').create();
 
+/**
+ * Handler Errors
+ */
+function onHandlerError() {
+    const args = Array.prototype.slice.call(arguments);
+
+    notify.onError({
+        title: 'Gulp Compile Error',
+        message: '<%= error.message %>',
+    }).apply(this, args);
+
+    this.emit('end');
+};
+
+
+/* Instructions */
 
 /**
- * Instructions
+ * Clean files in folder
+ * @param {function} cb Callback function
  */
 const clean = (cb) => {
     del([
@@ -23,6 +41,11 @@ const clean = (cb) => {
     return cb();
 };
 
+
+/**
+ * Run server with function hotreload
+ * @param {function} cb Callback function
+ */
 const serve = (cb) => {
     browserSync.init({
         server: {
@@ -33,27 +56,44 @@ const serve = (cb) => {
     return cb();
 };
 
+
+/**
+ * Generate html files
+ * @param {function} cb Callback function
+ */
 const html = (cb) => {
     src('src/views/*.pug')
-        .pipe(pug())
+        .pipe(pug()
+            .on('error', onHandlerError))
         .pipe(dest('dist'))
         .pipe(browserSync.stream());
 
     return cb();
 };
 
+
+/**
+ * Generate css files
+ * @param {function} cb Callback function
+ */
 const css = (cb) => {
     src([
         'src/stylus/*.styl',
         'src/stylus/*.stylus',
     ])
-        .pipe(stylus())
+        .pipe(stylus()
+            .on('error', onHandlerError))
         .pipe(dest('dist/css'))
         .pipe(browserSync.stream());
 
     return cb();
 };
 
+
+/**
+ * Start monitoring changes in files
+ * @param {function} cb Callback function
+ */
 const watches = (cb) => {
     watch('**/*.pug', html);
     watch([
@@ -65,9 +105,7 @@ const watches = (cb) => {
 };
 
 
-/**
- * Exports tasks
- */
+/* Export tasks */
 exports.clean = clean;
 exports.serve = serve;
 exports.html = html;
